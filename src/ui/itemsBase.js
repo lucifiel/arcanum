c8991c57382788e38ae1bc12d2d292eb7708169a
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2019-07-08 03:35:35 -0700

    more fixes to lock functions.

--- src/ui/itemsBase.js
+++ src/ui/itemsBase.js
@@ -26,14 +26,14 @@ export default {
 		 * @param {*} it 
 		 */
 		reslocked( it ) {
-			return it.disabled === true ||
-			( it.locked > 1 || !Game.tryUnlock(it) );
+			return it.disabled === true || it.locked > 1 ||
+			( it.locked === 1 && !Game.tryUnlock(it) );
 		},
 
 		locked(it) {
 
-			return it.disabled === true || it.maxed() ||
-			( it.locked >= 1 || !Game.tryUnlock(it) );
+			return it.disabled === true || it.maxed() || it.locked>1 ||
+			( it.locked === 1 && !Game.tryUnlock(it) );
 
 		}
 
