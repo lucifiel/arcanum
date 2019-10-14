1131c3503c33069f7269ef37a796f516bc1cf598
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2019-10-14 02:46:17 -0700

    spellist name.

--- src/composites/spelllist.js
+++ src/composites/spelllist.js
@@ -37,7 +37,7 @@ export default class SpellList extends Inventory {
 
 		super(vars);
 
-		if ( !this.name ) this.name = 'spelllist';
+		this.name = this.id = this.type = 'spelllist';
 		this.lastInd = this.lastInd || 0;
 
 		this.spaceProp = 'level';
