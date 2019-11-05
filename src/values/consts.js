471527fbee05723dc1eab423e9a6873c3495e60b
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2019-11-04 17:22:08 -0800

    constants work.

--- src/values/consts.js
+++ src/values/consts.js
@@ -11,28 +11,6 @@ const schoolMap = {
 schoolMap.arcane = schoolMap.lore = schoolMap.mana;
 
 
-/**
- * Conversion of constants to school.
- */
-export const SchoolConvert = {
-
-	arcane:'mana',
-	swamp:['shadow','water'],
-	plains:'light',
-	woods:'nature',
-	mountains:['fire','earth','air'],
-	hills:['earth','air'],
-	cave:['shadow','earth'],
-	sea:'water',
-	town:[ 'blood', 'mana'],
-	humanoid:'mana',
-	undead:'shadow',
-	dragon:'fire',
-	beast:'nature',
-	magicbeast:['mana','nature']
-
-};
-
 
 /**
  * @property {.<string,string>} schoolName - maps school to display name.
@@ -56,11 +34,127 @@ const WEARABLE = 'wearable';
 const MONSTER = 'monster';
 const HOME = 'home';
 const ARMOR = 'armor', WEAPON = 'weapon';
+
 const DUNGEON = 'dungeon';
 const LOCALE = 'locale';
 const EXPLORE = 'explore';
+const RAID = 'raid';
 
-export const RAID = 'raid';
-
-export { DUNGEON, EXPLORE, LOCALE };
+export { RAID, DUNGEON, EXPLORE, LOCALE };
 export { HOME, RESOURCE, NPC, SKILL, ENCOUNTER, WEARABLE, MONSTER, ARMOR, WEAPON };
+
+/**
+ * @const {.<string,string|string[]>} SchoolTable - converts string to associated school.
+ */
+export const SchoolTable = {
+
+	arcane:'mana',
+	swamp:['shadow','water'],
+	plains:'light',
+	woods:'nature',
+	mountains:['fire','earth','air'],
+	hills:['earth','air'],
+	cave:['shadow','earth'],
+	sea:'water',
+	town:[ 'blood', 'mana'],
+	humanoid:'mana',
+	undead:'shadow',
+	dragon:'fire',
+	beast:'nature',
+	magicbeast:['mana','nature']
+
+};
+
+/**
+ * base resource to buy item by school/kind.
+ * @param {string} school
+ * @param {number} [tier=1]
+ */
+export const getSchoolResource = ( school, tier=1 ) =>{
+
+	let table = SchoolCostTable[school];
+	if ( table ) {
+
+		table = table[tier];
+		if ( table ) return table;
+
+	}
+
+	// default
+	table = SchoolCostTable['other'];
+	return table[tier] || 'gems';
+
+}
+
+/**
+ * @const SchoolCostTable School-> Loot Tier-> Base Resource Cost.
+ */
+export const SchoolCostTable = {
+
+	shadow:{
+
+		0:'gems',
+		1:'shadowgem'
+
+	},
+	mana:{
+
+		0:'gems',
+		1:'managem'
+
+	},
+	nature:{
+
+		0:'gems',
+		1:'managem'
+
+	},
+	fire:{
+
+		0:'gems',
+		1:'managem'
+
+	},
+	earth:{
+
+		0:'gems',
+		1:'managem'
+
+	},
+	water:{
+
+		0:'gems',
+		1:'managem'
+
+	},
+	air:{
+
+		0:'gems',
+		1:'managem'
+
+	},
+	spirit:{
+
+		0:'gems',
+		1:'managem'
+
+	},
+	light:{
+		0:'gems',
+		1:'managem'
+
+	},
+	blood:{
+
+		0:'gems',
+		1:'managem'
+
+	},
+	other:{
+		0:'gems',
+		1:'managem',
+		2:'runestones'
+	}
+
+
+}
