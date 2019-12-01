c0f5b868c23c63ab0841c8d6641850b399594bf2
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2019-12-01 12:50:06 -0800

    allow loading of rares data list.

--- src/dataLoader.js
+++ src/dataLoader.js
@@ -232,6 +232,8 @@ export default {
 		if ( lists.encounters ) inst.encounters = this.initItems( items, lists['encounters'], Encounter, ENCOUNTER, ENCOUNTER);
 		if ( lists.monsters ) inst.monsters = this.initItems( items, lists['monsters'], Monster, MONSTER, MONSTER );
 
+		if ( lists.rares ) inst.rares = this.initItems( items, lists['rares'], Item );
+
 		if ( lists.locales ) this.initItems( items, lists['locales'], Locale );
 		if ( lists.dungeons ) this.initItems( items, lists['dungeons'], Dungeon );
 		if ( lists.spells ) this.initItems( items, lists['spells'], Spell );
