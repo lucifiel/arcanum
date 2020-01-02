f5a8355fdc3ad9acaf5660270a6f78ab9d83cb55
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2020-01-01 18:51:12 -0800

    just puppet stuff.

--- src/debug/debug.js
+++ src/debug/debug.js
@@ -1,4 +1,5 @@
 import Resource from "../items/resource";
+import RevStat from "../items/revStat";
 
 const ALL = 'all';
 const ALL_ALT = '*';
@@ -107,7 +108,7 @@ export default class Debug {
 		for( let p in res ){
 
 			var r = res[p];
-			if ( !r.locked && r instanceof Resource ){
+			if ( !r.locked && !(r instanceof RevStat) ){
 				this.game.fillItem( r )
 			}
 
