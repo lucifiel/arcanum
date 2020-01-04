fb3cb34cfb9c5abd8aa2ea4fec8786ad0a4578a9
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2020-01-04 05:12:15 -0800

    fixed some unlock requirements , floorspace now hidden unlock.

---
+++ src/items/furniture.js
@@ -0,0 +1,20 @@
+import GData from "./gdata";
+
+export default class Furniture extends GData {
+
+	constructor(vars) {
+
+		super(vars);
+		this.type = 'furniture';
+
+	}
+
+	/**
+	 * Tests whether item fills unlock requirement.
+	 * @returns {boolean}
+	 */
+	fillsRequire(){
+		return this.locked === false && this.value > 0;
+	}
+
+}
