2b92aaf3d9221d2635b5083a32a07ddc109aa482
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2020-01-14 08:32:48 -0800

    removing duplicate cloneClass func incorrectly merged.

--- src/util/dataUtil.js
+++ src/util/dataUtil.js
@@ -1,6 +1,5 @@
 import { clone, cloneClass } from "objecty";
 import Stat from "../values/stat";
-import { cloneClass } from "./util";
 
 /**
  * Find and return first element of set matching predicate.
